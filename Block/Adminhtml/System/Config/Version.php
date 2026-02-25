0c6c033690265bc993b47cfb8b22dc515ef25227
kuldip-ladola <kuldip1241@gmail.com>
2026-02-25 23:28:03 +0530

    change version from 1.0.10 to 1.0.11

--- Block/Adminhtml/System/Config/Version.php
+++ Block/Adminhtml/System/Config/Version.php
@@ -15,7 +15,7 @@ class Version extends Template implements RendererInterface
     public function render(AbstractElement $element)
     {
         // Replace '1.0.0' with your actual module version
-        $version = '1.0.10';
+        $version = '1.0.11';
         return '<div><strong>' . __('Version: %1', $version) . '</strong></div>';
     }
 }
